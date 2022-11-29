881c715aaa130788e9b09bb7394161d730f11179
csh44017 <csh44017@naver.com>
2022-11-30 00:06:35 +0900

    upload

---
+++ Python3/Lv_1/3진법 뒤집기/solution1.py
@@ -0,0 +1,16 @@
+import string
+
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0:
+        return tmp[r] 
+    else:
+        return convert(q, base) + tmp[r]
+
+def solution(n):
+    n_str = ""
+    n = convert(n, 3)
+    for num in reversed(n):
+        n_str += num
+    return int(n_str, 3)
