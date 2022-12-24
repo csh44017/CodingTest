21aba3625f6724097e7c3fa319c5e831a08a5b72
csh44017 <csh44017@naver.com>
2022-12-24 23:46:29 +0900

    upload

---
+++ Python3/Lv_2/124 나라의 숫자/solution1.py
@@ -0,0 +1,11 @@
+def radix(n, q):
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+
+    return rev_base[::-1]
+
+def solution(n):
+    print(radix(n, 3))
